e3b6c8e75a3263cf0afa367a296d6f4d2f5783a2
kadenk <kadenk@iastate.edu>
2021-03-18 23:29:28 -0500

    Created AudioRecorder class

---
+++ AudioRecorder.js
@@ -0,0 +1,127 @@
+class AudioRecorder {
+	/**
+	 * 
+	 * @param {{
+	 * 	onAudioStart: function,
+	 * 	onPermissionsFail: function,
+	 * 	onAudioUpdate: function(Float32Array),
+	 * 	onAudioStop: function(Blob)
+	 * }} callbacks 
+	 */
+	constructor(callbacks) {
+		this.audioBlobs = [];
+		this.callbacks = callbacks;
+		/**
+		 * @type {{
+		 * 	stream: MediaStream,
+		 * 	processor: ScriptProcessorNode,
+		 * 	mediaRecorder: MediaRecorder
+		 * }}
+		 */
+		this.trackedObjects = {
+			stream: undefined,
+			processor: undefined,
+			mediaRecorder: undefined
+		};
+	}
+
+	/**
+	 * 
+	 * @param {string} callbackName 
+	 * @param {function} callbackFunction 
+	 */
+	addCallback(callbackName, callbackFunction) {
+		this.callbacks[callbackName] = callbackFunction;
+	}
+
+	async start() {
+		await this.requestStream();
+		if (this.trackedObjects.stream) {
+			if (this.startRecordings(this.trackedObjects.stream))
+				this.callbacks.onAudioStart();
+		}
+	}
+
+	async stop() {
+		if (this.trackedObjects.mediaRecorder)
+			this.trackedObjects.mediaRecorder.stop();
+		if (this.trackedObjects.stream) {
+			this.trackedObjects.stream.getAudioTracks().forEach((track) => {
+				track.stop();
+			});
+		}
+		if (this.trackedObjects.processor)
+			this.trackedObjects.processor.disconnect();
+	}
+
+
+	async requestStream() {
+		if (this.stream) return;
+		
+		let microphonePermissions = navigator.permissions.query({name:'microphone'});
+		if (microphonePermissions.state == 'denied') {
+			console.error("Blocked from accessing audio input. Please consider changing microphone permissions.");
+			if (this.callbacks.onPermissionsFail)
+				this.callbacks.onPermissionsFail();
+			return;
+		}
+	
+		// Request audio stream
+		let stream 
+		try {
+			stream = await navigator.mediaDevices.getUserMedia({
+				audio: true,
+				video: false,
+			});
+			console.log(stream.getTracks());
+		}
+		catch (e) {
+			console.error(e + " – Could not access audio input.");
+			if (this.callbacks.onPermissionsFail)
+				this.callbacks.onPermissionsFail();
+			return;
+		}
+
+		this.trackedObjects.stream = stream;
+	}
+	async startRecordings(stream) {
+		const audioContext = new AudioContext();
+		const source = audioContext.createMediaStreamSource(stream);
+		const processor = audioContext.createScriptProcessor(1024, 1, 1);
+		processor.onaudioprocess = (e) => this.onAudioUpdate(e);
+	
+		/** @type{MediaRecorder} */
+		const mediaRecorder = await new MediaRecorder(stream);			
+		mediaRecorder.addEventListener("dataavailable", (e) => this.onDataAvailableRecorder(e));
+		mediaRecorder.addEventListener("stop", (e) => this.onStopMediaRecorder(e));
+		
+		source.connect(processor);
+		processor.connect(audioContext.destination);
+		mediaRecorder.start();
+		
+		this.trackedObjects.processor = processor;
+		this.trackedObjects.mediaRecorder = mediaRecorder;
+
+		return true;
+	}
+
+	/**
+	 * @param {AudioProcessingEvent} e 
+	 */
+	async onAudioUpdate(e) {
+		let audioBuffer = e.inputBuffer;
+		let audioChannel = new Float32Array(new Array(audioBuffer.length));
+		audioBuffer.copyFromChannel(audioChannel, 0);
+		if (this.callbacks.onAudioUpdate)
+			this.callbacks.onAudioUpdate(audioChannel);
+	}
+
+	async onDataAvailableRecorder(e) {
+		this.audioBlobs.push(e.data); // push audio Blob
+	}
+	async onStopMediaRecorder(e) {
+		const audioBlob = new Blob(this.audioBlobs);
+		if (this.callbacks.onAudioStop)
+			this.callbacks.onAudioStop(audioBlob);
+	}
+}
