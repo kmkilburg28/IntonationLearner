1a9c218f708ce0fb35b7af187fb81e7cd016654a
aprilmelody93 <59852275+aprilmelody93@users.noreply.github.com>
2021-04-07 18:06:03 -0500

    Update Normalization.js

--- Normalization.js
+++ Normalization.js
@@ -1,4 +1,13 @@
+
+let normalizedArray = [];
+
 function NormalizePitch(pitchArray) {
-	// do stuff
+	maxPitch = Math.max(...pitchArray);
+	minPitch = Math.min(...pitchArray);
+
+	for (i=0; i<pitchArray.length; i++) {
+		normalizedPitch = (pitchArray[i]-minPitch)/(maxPitch-minPitch);
+        normalizedArray.push(normalizedPitch);
+	}
 	return normalizedArray;
 }
