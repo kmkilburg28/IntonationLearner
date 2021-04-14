81c2697f5e6d96e5dd5c48258755e2ad1acffef6
gherring <gherring@iastate.edu>
2021-04-14 09:37:04 -0500

    DTW

--- DTW.js
+++ DTW.js
@@ -1,9 +1,15 @@
 function DTW(modelPitchArr, userPitchArr){
     modelPitchArr = trim(modelPitchArr);
     userPitchArr = trim(userPitchArr);
+	var len = 10;
+	var newPitchArr = new Float32Array(userPitchArr.length + len)
+	newPitchArr.set(userPitchArr, len);
+	console.log(newPitchArr);
+	userPitchArr = newPitchArr;
 	var M = new Array();
 	var d = new Array();
 	var parent = new Array();
+	var path = new Array();
 	for(var i = 0; i < userPitchArr.length; i++){
 		M[i] = new Array();
 		d[i] = new Array();
@@ -16,11 +22,13 @@ function DTW(modelPitchArr, userPitchArr){
 	}
 	M[0][0] = d[0][0];
 	for(var j = 1; j < modelPitchArr.length; j++){
-		M[0][j] = M[0][j - 1] + d[0][j]; 
+		M[0][j] = M[0][j - 1] + d[0][j];
+		parent[0][j] = {x:0,y:j-1};
 	}
 	for(var i = 1; i < userPitchArr.length; i++){
 		//console.log(i);
-		M[i][0] = M[i - 1][0] + d[i][0]; 
+		M[i][0] = M[i - 1][0] + d[i][0];
+		parent[i][0] = {x:i-1,y:0};
 	}
 
 	for(var i = 1; i < userPitchArr.length; i++){
@@ -28,19 +36,27 @@ function DTW(modelPitchArr, userPitchArr){
 			var min;
 			if(M[i - 1][j - 1] <= M[i][j - 1] && M[i - 1][j - 1] <= M[i][j - 1]){
 				min = M[i - 1][j - 1];
+				parent[i][j] = {x:i-1,y:j-1};
 			}else if (M[i][j - 1] <= M[i - 1][j - 1] && M[i][j - 1] <= M[i - 1][j]){
 				min = M[i][j - 1];
+				parent[i][j] = {x:i,y:j-1};
 			}else{
 				min = M[i - 1][j];
+				parent[i][j] = {x:i-1,y:j};
 			}
 			M[i][j] = d[i][j] + min;
 		}
 	}
-	for(var i = 1; i < userPitchArr.length; i++){
-		console.log(M[i]);
+	var k = 0;
+	var p = parent[userPitchArr.length - 1][modelPitchArr.length - 1];
+	while(p.x != 0 || p.y != 0){
+		path[k] = p;
+		p = parent[p.x][p.y];
+		k++;
 	}
-	
-	return M;
+	path[k] = p;
+
+	return path;
 }
 
 function trim(p1){
