53971268006f323d12ff52f38f7d75c65cafeb1b
gherring <gherring@iastate.edu>
2021-04-14 10:30:15 -0500

    corelation and MSE added and training recovered

---
+++ CorrelationMSE.js
@@ -0,0 +1,32 @@
+function correlationCoe(pitchArr, userPitchArr) {
+	var modelAvg = 0;
+	for (var i = 0; i < pitchArr.length; i++) {
+		modelAvg += pitchArr[i];
+	}
+	modelAvg /= pitchArr.length;
+	var userAvg = 0;
+	for (var i = 0; i < userPitchArr.length; i++) {
+		userAvg += userPitchArr[i];
+	}
+	userAvg /= userPitchArr.length;
+
+	var nume = 0;
+	var denomUser = 0;
+	var denomModel = 0;
+	for (var i = 0; i < userPitchArr.length; i++){
+		nume += (userPitchArr[i] - userAvg) * (pitchArr[i] - modelAvg);
+		denomUser += (userPitchArr[i] - userAvg) * (userPitchArr[i] - userAvg);
+		denomModel += (pitchArr[i] - modelAvg) * (pitchArr[i] - modelAvg);
+	}
+	var denom = denomUser * denomModel;
+	denom = Math.sqrt(denom);
+	return nume / denom;
+}
+
+function MSE(pitchArr, userPitchArr){
+	var ret = 0;
+	for (var i = 0; i < userPitchArr.length; i++) {
+		ret += (pitchArr[i] - userPitchArr[i]) * (pitchArr[i] - userPitchArr[i]);
+	}
+	return ret / userPitchArr.length;
+}
--- DTW.js
+++ DTW.js
@@ -1,11 +1,6 @@
 function DTW(modelPitchArr, userPitchArr){
     modelPitchArr = trim(modelPitchArr);
     userPitchArr = trim(userPitchArr);
-	var len = 10;
-	var newPitchArr = new Float32Array(userPitchArr.length + len)
-	newPitchArr.set(userPitchArr, len);
-	console.log(newPitchArr);
-	userPitchArr = newPitchArr;
 	var M = new Array();
 	var d = new Array();
 	var parent = new Array();
@@ -26,7 +21,6 @@ function DTW(modelPitchArr, userPitchArr){
 		parent[0][j] = {x:0,y:j-1};
 	}
 	for(var i = 1; i < userPitchArr.length; i++){
-		//console.log(i);
 		M[i][0] = M[i - 1][0] + d[i][0];
 		parent[i][0] = {x:i-1,y:0};
 	}
@@ -63,5 +57,53 @@ function trim(p1){
     p1Segment = ModelSegment(p1);
     p1 = p1.slice(p1Segment[0], p1Segment[p1Segment.length]);
 	return p1;
+}
+
+function warp(modelPitchArr, userPitchArr, flag){
+	modelPitchArr = trim(modelPitchArr);
+    userPitchArr = trim(userPitchArr);
+	var len = 10;
+	var newPitchArr = new Float32Array(userPitchArr.length + len)
+	newPitchArr.set(userPitchArr, len);
+	//console.log(newPitchArr);
+	userPitchArr = newPitchArr;
+	var dtwMap = DTW(modelPitchArr, userPitchArr);
+	warpedModel = new Array();
+	warpedUser = new Array();
+	var i = dtwMap.length - 1;
+	var j = dtwMap.length - 1;
+	var k = 0;
+	while(i > 0 && j > 0){
+		var mAvg = dtwMap[i].x;
+		var uAvg = dtwMap[j].y;
+		var uCount = 1;
+		var mCount = 1;
+		while((i > 0 && dtwMap[i].x == dtwMap[i - 1].x) || (j > 0 && dtwMap[j].y == dtwMap[j - 1].y)){
+			
+			if(dtwMap[j].y == dtwMap[j - 1].y){
+				j--;
+				uAvg += dtwMap[j].y;
+				uCount++;
+				if(j == 0){
+					break;
+				}
+			}
+			if(dtwMap[i].x == dtwMap[i - 1].x){
+				i--;
+				mAvg += dtwMap[i].x;
+				mCount++;
+				if(i == 0){
+					break;
+				}
+			}
+		}
+		warpedModel[k] = mAvg/mCount;
+		warpedUser[k] = uAvg/uCount;
+		k++;
+	}
+	if(flag > 0){
+		return warpedModel;
+	}
+	return warpedUser;
 }
 
