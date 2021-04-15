448a4440438daaff442081340c61b02a768de022
kadenk <kadenk@iastate.edu>
2021-04-14 23:18:03 -0500

    Bug fix for normalization not detecting mins

--- Normalization.js
+++ Normalization.js
@@ -5,12 +5,12 @@
 function NormalizeArray(array) {
 	let max = Math.max(...array);
 	let min = max;
-	for(let i = 0; i < array.length; i++){
-		if(array[i] > 0 && array[i] < min){
+	for(let i = 0; i < array.length; i++) {
+		if(array[i] > 0 && array[i] < min) {
 			min = array[i];
 		}
 	}
 	
 	let range = max - min;
-	return array.map(value => value > 0 ? (value - min) / range : 0);
+	return array.map(value => value > 0 ? (value - min) / range : -1);
 }
