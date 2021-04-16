b5a95440f64fc88056da28ba8615857534192a36
kadenk <kadenk@iastate.edu>
2021-04-16 10:19:36 -0500

    Fixed Co and MSE force real values

--- CorrelationMSE.js
+++ CorrelationMSE.js
@@ -1,22 +1,35 @@
+/**
+ * @param {number} value 
+ * @returns {number}
+ */
+function forceToReal(value) {
+	return value < 0 ? 0 : (value > 1 ? 1 : value);
+}
+/**
+ * @param {Array} a 
+ * @returns {Array}
+ */
 function removeNonReal(a){
-	a.map(value => isReal(value) ? 0: value);
-	return a;
+	return a.map(forceToReal);
 }
 
+/**
+ * @param {Array} pitchArr 
+ * @param {Array} userPitchArr 
+ * @returns {number}
+ */
 function correlationCoe(pitchArr, userPitchArr) {
 	pitchArr = removeNonReal(pitchArr);
 	userPitchArr = removeNonReal(userPitchArr);
-	console.log(pitchArr, userPitchArr);
+
 	var modelAvg = 0;
 
 	for (var i = 0; i < pitchArr.length; i++) {
-
 		modelAvg += pitchArr[i];
 	}
 	modelAvg /= pitchArr.length;
 	var userAvg = 0;
 	for (var i = 0; i < userPitchArr.length; i++) {
-		
 		userAvg += userPitchArr[i];
 	}
 	userAvg /= userPitchArr.length;
@@ -34,10 +47,17 @@ function correlationCoe(pitchArr, userPitchArr) {
 	return nume / denom;
 }
 
+/**
+ * @param {Array} array1
+ * @param {Array} array2
+ * @returns {number}
+ */
 function MSE(array1, array2) {
 	let ret = 0;
 	for (let i = 0; i < array1.length; ++i) {
-		ret += Math.pow(array1[1] - array2[i], 2);
+		let value1 = forceToReal(array1[i]);
+		let value2 = forceToReal(array2[i]);
+		ret += Math.pow(value1 - value2, 2);
 	}
 	return ret / array1.length;
 }
